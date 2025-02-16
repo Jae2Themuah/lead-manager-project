35a4bd6a7e36209fd937d68e66045ce25a3b7f68
Jae2Themuah <regimecompany@gmail.com>
2025-02-16 15:50:29 -0500

    Added styles.css and updated project files

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // If you have a global CSS file
+
+// Main App component
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to the React application!</p>
+    </div>
+  );
+}
+
+// Render the App component into the root div in your HTML
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
